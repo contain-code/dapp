f38306c4e9abd688ba7d0f9be5ae1aa86b067205
contain-code <tnmcong.20it7@vku.udn.vn>
2024-05-12 14:01:46 +0700

    completed filter component

---
+++ Components/Error/Error.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import Style from './Error.module.css'
+
+const Error = ({error}) => {
+  return (
+    // <div className={Style.Error}>
+    //   <div className={Style.Error_box}>
+    //     <h1>Please Fix This Error & Reload Browser</h1>
+    //     {error}
+    //   </div>
+    // </div>
+    <div className={Style.Error}>
+      hi
+    </div>
+  )
+}
+
+export default Error
