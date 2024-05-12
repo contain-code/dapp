8a27229781cd538ce3de9fa001d850d2d4a80c83
contain-code <tnmcong.20it7@vku.udn.vn>
2024-05-13 02:35:08 +0700

    complete ui

--- Components/Error/Error.jsx
+++ Components/Error/Error.jsx
@@ -4,15 +4,13 @@ import Style from './Error.module.css'
 
 const Error = ({error}) => {
   return (
-    // <div className={Style.Error}>
-    //   <div className={Style.Error_box}>
-    //     <h1>Please Fix This Error & Reload Browser</h1>
-    //     {error}
-    //   </div>
-    // </div>
     <div className={Style.Error}>
-      hi
+      <div className={Style.Error_box}>
+        <h1>Please Fix This Error & Reload Browser</h1>
+        {error}
+      </div>
     </div>
+    
   )
 }
 
